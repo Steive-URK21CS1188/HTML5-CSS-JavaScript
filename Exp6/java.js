4d7e0c661d12686758529c84579449827dbb2c66
Steive-URK21CS1188 <110010600+Steive-URK21CS1188@users.noreply.github.com>
2024-09-01 19:40:40 +0530

    Add files via upload

---
+++ Exp6/java.js
@@ -0,0 +1,30 @@
+            
+            function bgc(){
+                var a1=document.getElementById('bg').value;
+                document.getElementById('e6').style.backgroundColor=a1
+            }
+            function tc(){
+                var a2=document.getElementById('tc').value;
+                document.getElementById('e6').style.color=a2
+            }
+            function ts(){
+                var a3=document.getElementById('s').value;
+                document.getElementById('e6').style.fontSize=a3+"%"
+            }
+            function bh(){
+                var a4=document.getElementById('h').value;
+                document.getElementById('e6').style.height=a4+"cm"
+            }
+            function bw(){
+                var a5=document.getElementById('w').value;
+                document.getElementById('e6').style.width=a5+"%"
+            }
+            function brd(){
+                var a6=document.getElementById('rd').value;
+                document.getElementById('e6').style.borderRadius=a6+"%"
+            }
+            function opa(){
+                var a7=document.getElementById('op').value;
+                document.getElementById('e6').style.opacity=a7
+            }
+            
